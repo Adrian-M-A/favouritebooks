4b596ed731d4e1a4ba456dff06e6377ed2916d24
Adrian M.A <adrian@neurocadi.es>
2020-08-27 11:54:59 +0200

    Back Books CRUD ready

--- back/src/config/mongoose.js
+++ back/src/config/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = 'mongodb+srv://Adrian:s0c19lnt@cluster0.u9ok9.mongodb.net/socialnetwork';
+const MONGO_URI = 'mongodb+srv://Adrian:qQ1!wwww@cluster0.abpnb.mongodb.net/';
 
 const db = mongoose.connect(MONGO_URI, {
     useNewUrlParser:true,
